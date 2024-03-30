a3c48333a8c327c4f8191386f848e81d3610c356
Arsalan Sheikh <160051126+arsalansheikh07@users.noreply.github.com>
2024-03-30 23:09:49 +0500

    commit

---
+++ main.js
@@ -0,0 +1,2 @@
+var Name = " Arsalan_Sheikh";
+console.log("Hello ".concat(Name, ", Would You Like To Learn Some Typescript Today?"));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//                               Day 2 Question Complete
+
+let Name: string = " Arsalan_Sheikh";
+
+console.log(`Hello ${Name}, Would You Like To Learn Some Typescript Today?`);
